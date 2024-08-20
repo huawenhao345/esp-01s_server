ad19574795c84a3601f635c391ea53320900ed41
Point Hua <huawenhaohhh@126.com>
2024-08-20 15:52:27 +0800

    try1

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
 
 const int serverPort = 5000;
 
-const char *ssid = "esp_01swifi";
 const char *password = "12345678";
+const char *ssid = "esp_01swifi";
+
+// 创建WiFiServer对象并指定端口号
+WiFiServer server(5000); // 端口号5000
 
 void setup() {
   Serial.begin(115200);
@@ -14,11 +18,11 @@ void setup() {
   Serial.println(WiFi.softAPIP()); // 打印AP的IP地址
 
   // 启动TCP服务器
-  Server.begin();
+  server.begin();
 }
 
 void loop() {
-  WiFiClient client = Server.available(); // 检查是否有客户端连接
+  WiFiClient client = server.available(); // 检查是否有客户端连接
   if (client) {
     Serial.println("新客户端连接");
     while (client.connected()) {
